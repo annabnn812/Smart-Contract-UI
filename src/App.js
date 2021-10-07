d611ccec98b9597d3afe962f79d15b5f2a4d92b6
annaspb <86491435+annabnn812@users.noreply.github.com>
2021-10-07 18:33:03 +0000

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,163 @@
+import React, { useEffect, useState } from "react";
+import { ethers } from "ethers";
+import './App.css';
+import newApp from './utils/newApp.json';
+
+const App = () => {
+  const [currentAccount, setCurrentAccount] = useState("");
+  const [allGifts, setAllGifts] = useState([]);
+  const contractAddress = "0x03e5b36001A61a9D02cDA669c0Ac5386E2B9388A";
+
+ const getAllGifts = async () => {
+    try {
+      if (window.ethereum) {
+        const provider = new ethers.providers.Web3Provider();
+        const signer = provider.getSigner();
+        const newAppContract = new ethers.Contract(contractAddress, newApp.abi, signer);
+
+        const waves = await newAppContract.getAllGifts();
+
+        let wavesCleaned = [];
+        waves.forEach(wave => {
+          wavesCleaned.push({
+            address: wave.waver,
+            timestamp: new Date(wave.timestamp * 1000),
+            message: wave.message
+          });
+        });
+
+        setAllGifts(wavesCleaned);
+      } else {
+        console.log("Ethereum object doesn't exist!")
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  
+  const checkIfWalletIsConnected = async () => {
+    try {
+      const {ethereum} = window;
+
+      if (!ethereum) {
+        console.log("Make sure you have metamask!");
+        return;
+      } else {
+        console.log("We have the ethereum object", ethereum);
+      }
+
+      const accounts = await ethereum.request({ method: 'eth_accounts' });
+
+      if (accounts.length !== 0) {
+        const account = accounts[0];
+        console.log("Found an authorized account:", account);
+        setCurrentAccount(account);
+      } else {
+        console.log("No authorized account found")
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  /**
+  * Implement your connectWallet method here
+  */
+  const connectWallet = async () => {
+    try {
+      const { ethereum } = window;
+
+      if (!ethereum) {
+        alert("Get MetaMask!");
+        return;
+      }
+
+      const accounts = await ethereum.request({ method: "eth_requestAccounts" });
+
+      console.log("Connected", accounts[0]);
+      setCurrentAccount(accounts[0]); 
+    } catch (error) {
+      console.log(error)
+    }
+  }
+  const wave = async () => {
+    try {
+      const { ethereum } = window;
+
+      if (ethereum) {
+        const provider = new ethers.providers.Web3Provider(ethereum);
+        const signer = provider.getSigner();
+        const newAppContract = new ethers.Contract(contractAddress, newApp.abi, signer);
+
+        let count = await newAppContract.getTotalGifts();
+        console.log("Retrieved total wave count...", count.toNumber());
+
+        const waveTxn = await newAppContract.wave();
+        console.log("Mining...", waveTxn.hash);
+
+        await waveTxn.wait();
+        console.log("Mined -- ", waveTxn.hash);
+
+        count = await newAppContract.getTotalGifts();
+        console.log("Retrieved total wave count...", count.toNumber());
+      } else {
+        console.log("Ethereum object doesn't exist!");
+      }
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
+
+  useEffect(() => {
+    checkIfWalletIsConnected();
+  }, [])
+  
+  return (
+    
+      <div className="dataContainer">
+       
+         <div className="App">
+          <div className="container">
+           <div className="header-container">
+       
+       
+
+        <div className="bio">
+          Connect your Ethereum wallet and send crypto!
+          
+        </div>
+        <div> </div>
+        {/*
+        * If there is no currentAccount render this button
+        */}
+
+        {!currentAccount && (
+          <button className="connectButton" onClick={connectWallet}>
+            Connect Wallet
+          </button>
+         )}
+
+        <button className="sendButton" onClick={null}>
+          Send crypto
+        </button>
+        
+        {allGifts.map((wave, index) => {
+          return (
+            <div style={{ backgroundColor: "OldLace", marginTop: "16px", padding: "8px" }}>
+              <div>Address: {wave.address}</div>
+              <div>Time: {wave.timestamp.toString()}</div>
+              <div>Message: {wave.message}</div>
+            </div>)
+        })}
+
+        
+      </div>
+    </div>
+    </div>
+    </div>
+  );
+}
+
+export default App
